e9dc33ca4408bf2da9bc4809bd6e2e133acc25e4
utraskevice <ujuozenaite@gmail.com>
2022-09-21 16:09:47 +0200

    Remove function

--- src/script.js
+++ src/script.js
@@ -8,12 +8,34 @@ function render() {
   toDoList.innerHTML = null;
   toDoList.id = toDoId;
 
-  toDo.forEach((singleObject) => {
+  toDo.forEach((singleObject, index) => {
     // console.log("Draw:", singleObject);
     const object = document.createElement("li");
+    const iconDiv = document.createElement("div");
+    const removeEl = document.createElement("button");
+    const starEl = document.createElement("button");
+    const editEl = document.createElement("button");
 
     object.textContent = singleObject.newInput;
+    object.className = "single-object";
+    object.setAttribute("draggable", true);
+    editEl.innerHTML = `<i class="fa-solid fa-pen"></i>`;
+    editEl.className = "icon";
+    starEl.innerHTML = `<i class="fa-solid fa-star"></i>`;
+    starEl.classList.add("icon");
+    removeEl.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+    removeEl.className = "icon";
 
+    // Remove function
+
+    removeEl.addEventListener("click", () => {
+      toDo.splice(index, 1);
+      window.localStorage.setItem("toDo", JSON.stringify(toDo));
+      render();
+    });
+
+    iconDiv.append(removeEl, starEl, editEl);
+    object.append(iconDiv);
     toDoList.append(object);
   });
 
